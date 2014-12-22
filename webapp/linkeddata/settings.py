0161806b43263a3ddd5924e855767bda76b0d7b3
Javier G. Sogo <javi@web.(none)>
2014-12-22 19:22:14 +0100

    static path, ignore

--- webapp/linkeddata/settings.py
+++ webapp/linkeddata/settings.py
@@ -88,3 +88,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+try:
+	from local_settings import *
+except ImportError:
+	pass
