b952bc003a35dc503ba9f8cdff445c7188f50052
Alfonso.LT <alf.lt@hotmail.com>
2014-12-23 20:33:22 +0100

    Actualización allergen y diseases

--- webapp/allergen/models.py
+++ webapp/allergen/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class AllergenManager(models.Manager):
+    def create_from_list(self, list):
+        instance = self.model()
+        instance.Group = list[0]
+        instance.Main = list[1]
+        instance.Type = list[2]
+        instance.Species = list[3]
+        instance.Common = list[4]
+        instance.IUIS = list[5]
+        instance.save()
+        return instance
+
+class Allergen(models.Model):
+    filename = 'ALLERGEN'
+
+    Group   = models.CharField(max_length=100, primary_key=True)
+    Main    = models.CharField(max_length=10, choices=(('Non Food', 'Non Food Allergen'), ('Food','Food Allergen')))
+    Type    = models.CharField(max_length=100)
+    Species = models.CharField(max_length=100)
+    Common  = models.CharField(max_length=100)
+    IUIS    = models.CharField(max_length=25)
+
+    objects = AllergenManager()
+
--- webapp/diseases/models.py
+++ webapp/diseases/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class DiseaseManager(models.Manager):
+    def create_from_list(self, list):
+        instance = self.model()
+        instance.Disease = list[0]
+        instance.Description = list[1]
+        instance.Food = list[2]
+        instance.Nutrient = list[3]
+        instance.Allergen = list[4]
+        instance.save()
+        return instance
+
+class Disease(models.Model):
+    filename = 'DISEASE'
+
+    Disease     = models.CharField(max_length=255, primary_key=True)
+    Description = models.TextField(null=True, blank=True)
+    Food        = models.CharField(max_length=100)
+    Nutrient    = models.CharField(max_length=100)
+    Allergen    = models.CharField(max_length=100)
+
+    objects = DiseaseManager()
+
