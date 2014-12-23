bdb47d90cfd17a703893e504ff59c5fe90bcd859
Alfonso.LT <alf.lt@hotmail.com>
2014-12-23 12:04:59 +0100

    add admin Allergen

--- webapp/recipes/models.py
+++ webapp/recipes/models.py
@@ -5,7 +5,7 @@ from food.models import Food
 
 class Recipe(models.Model):
     title = models.CharField(max_length=255)
-    description = mdoels.TextField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
 
     produces = models.ForeignKey(Food)
     yields = models.IntegerField(null=True, blank=True)
