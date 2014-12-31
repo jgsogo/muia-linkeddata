0ffb34e3e2df39af558740bd207e78959d348a41
Javier G. Sogo <jgsogo@gmail.com>
2014-12-31 11:34:53 +0100

    migraciones (he tenido que resetearlas, porque lo de cambiar foreign_keys hay que hacerlo con más cuidado) y cambios en el R2RML correspondiente

--- webapp/diseases/migrations/0001_initial.py
+++ webapp/diseases/migrations/0001_initial.py
@@ -7,8 +7,8 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
+        ('food', '0002_auto_20141230_1004'),
         ('allergen', '0001_initial'),
-        ('food', '0001_initial'),
         ('nutrients', '0001_initial'),
     ]
 
@@ -25,7 +25,8 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Disease',
             fields=[
-                ('DiseaseName', models.CharField(max_length=100, serialize=False, primary_key=True)),
+                ('DiseaseKey', models.CharField(max_length=100, serialize=False, primary_key=True)),
+                ('DiseaseName', models.CharField(max_length=100)),
                 ('Description', models.TextField(null=True, blank=True)),
                 ('DOID', models.CharField(max_length=15, null=True, blank=True)),
                 ('AlleList', models.ManyToManyField(to='allergen.Allergen', through='diseases.AllergenCausesDisease')),
@@ -38,7 +39,7 @@ class Migration(migrations.Migration):
             name='FoodCausesDisease',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('DiseaseName', models.ForeignKey(to='diseases.Disease')),
+                ('DiseaseKey', models.ForeignKey(to='diseases.Disease')),
                 ('NDB_No', models.ForeignKey(to='food.Food')),
             ],
             options={
@@ -49,8 +50,8 @@ class Migration(migrations.Migration):
             name='NutrientCausesDisease',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('DiseaseName', models.ForeignKey(to='diseases.Disease')),
-                ('Nutr_No', models.ForeignKey(to='nutrients.Nutrient')),
+                ('DiseaseKey', models.ForeignKey(to='diseases.Disease')),
+                ('NutrientID', models.ForeignKey(to='nutrients.Nutrient')),
             ],
             options={
             },
@@ -70,7 +71,7 @@ class Migration(migrations.Migration):
         ),
         migrations.AddField(
             model_name='allergencausesdisease',
-            name='DiseaseName',
+            name='DiseaseKey',
             field=models.ForeignKey(to='diseases.Disease'),
             preserve_default=True,
         ),
