ee9cb385825955e99a41d51eece33cf59e79cb5d
Javier G. Sogo <jgsogo@gmail.com>
2015-01-01 14:12:34 +0100

    propuesta matching ingredientes

--- webapp/food/models/food.py
+++ webapp/food/models/food.py
@@ -1,4 +1,7 @@
+
+import re
 from django.db import models
+from collections import Counter
 
 class FoodManager(models.Manager):
 
@@ -15,13 +18,32 @@ class FoodManager(models.Manager):
         #! TODO: Look for the best candidate based on 'name'
         #   - single_instance = True ==> return just one instance (or None)
         #   - single_instance = False ==> return a list/queryset of instances (can be empty)
-        if single_instance:
-            try:
-                return self.get(Shrt_Desc=name)
-            except self.model.DoesNotExist:
-                return None
-        else:
-            return self.filter(Shrt_Desc=name)
+
+        def match_function(str, counter, delta=1):
+            qs = self.filter(Long_Desc__icontains=str).values_list('NDB_No', 'Long_Desc')
+            for item in qs:
+                ndb_no = item[0]
+                #n_desc = len(re.findall(r"[\w']+", item[1]))
+                counter[ndb_no] += delta#/n_desc
+
+        items = re.findall(r"[\w']+", name)
+        n_items = len(items)        
+        counter = Counter()
+        for i in xrange(n_items, 0, -1):
+            #print '\niteration ', i
+            for j in xrange(n_items-i+1):
+                str = ' '.join(items[j:j+i])
+                #print "%r" % str
+                match_function(str, counter, i)
+            #print '\t > ', counter.most_common(10)
+
+        if single_instance and len(counter):
+            ndb_no = counter.most_common(1)[0][0]
+            return self.get(NDB_No=ndb_no)
+        elif not single_instance:
+            ndb_no = [it[0] for it in counter.most_common(10)]
+            return self.filter(NDB_No__in=ndb_no)
+        return None
 
 
 class Food(models.Model):
